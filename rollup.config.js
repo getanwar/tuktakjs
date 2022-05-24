78c1365fb753e3d7237c5af13cb2c6ac46c4f5cd
Anwar Hussain <anwar828@gmail.com>
2022-05-24 16:42:37 +0600

    add eslint support

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,24 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import { eslint } from "rollup-plugin-eslint";
-import { uglify } from "rollup-plugin-uglify";
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import { eslint } from 'rollup-plugin-eslint';
+import { uglify } from 'rollup-plugin-uglify';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 export default {
-  input: "src/index.js",
-  output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" },
-  ],
-  plugins: [
-    resolve(),
-    babel({
-      exclude: "node_modules/**",
-    }),
-    commonjs(),
-  ],
+    input: 'src/index.js',
+    output: [
+        { file: pkg.main, format: 'cjs' },
+        { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+        eslint(),
+        resolve(),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        commonjs(),
+        uglify(),
+    ],
 };
